db1f4ed29cc4ba17f2866183cbc5597f0a6e4c86
Mark Reinke <mark.w.reinke.51@gmail.com>
2022-10-30 17:29:20 -0400

    fix game

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -18,39 +18,33 @@ class MinesweeperGame() {
         gameBoard = MinesweeperBoard(num)
     }
 
-    fun viewBoard() {
-        gameBoard.oldDisplayBoard()
-    }
-
-    fun calculateWin(): Boolean {
-        if(gameBoard.oldBoard == gameBoard.visibleBoard)  {
-            println("Congratulations! You found all the mines!")
-            return true
-        } else {
-            return false
-        }
-    }
-
-    fun markSpot(x: Int, y: Int): Boolean {
-        if(x - 1 >= 0
-            && x - 1 < gameBoard.X_SIZE
-            && y - 1 >= 0
-            && y - 1 < gameBoard.Y_SIZE) {
-            gameBoard.markMineInVisible(x - 1, y - 1)
-            viewBoard()
-        }
-
-        return calculateWin()
-    }
-
     fun play() {
         initiateGame()
-        viewBoard()
         var won = false
-        while(!won) {
-            println("Set/delete mines marks (x and y coordinates):")
-            val (inX, inY) = readln().split(" ").map{it.toInt()}
-            won = markSpot(inX, inY)
+        var ended = false
+        while(!ended) {
+            gameBoard.displayBoard()
+            println("Set/unset mines marks or claim a cell as free:")
+            val (inX, inY, action) = readln().split(" ")
+            val x = inY.toInt() - 1
+            val y = inX.toInt() - 1
+            println(action == "mine")
+            if(action == "mine") {
+                won = gameBoard.markSpot(x, y)
+                ended = won
+            } else if(action == "free") {
+                ended = !gameBoard.explore(x, y)
+                if(gameBoard.calculateWin()) {
+                    won = true
+                    ended = true
+                }
+            }
+        }
+        gameBoard.displayBoard()
+        if(!won) {
+            println("You stepped on a mine and failed!")
+        } else {
+            println("Congratulations! You found all the mines!")
         }
     }
 }
@@ -59,57 +53,58 @@ class MinesweeperGame() {
 class MinesweeperBoard(val numMines: Int, val X_SIZE: Int = 9, val Y_SIZE: Int = 9) {
     class Tile(val x: Int, val y: Int, var mined: Boolean = false) {
         val MINE_TILE: Char = 'x'
-        val SAFE_TILE: Char = '.'
+        val UNEXPLORED_TILE: Char = '.'
         val MARKED_TILE: Char = '*'
-        val EXPLORED_TILE: Char = '/'
+        val SAFE_TILE: Char = '/'
 
-        var display: Char = SAFE_TILE
+        var display: Char = UNEXPLORED_TILE
         var numMines: Int = 0
         var marked: Boolean = false
             set(value) {
                 if(!explored) {
                     field = value
-                    if (value) {
-                        display = MARKED_TILE
-                    }
+                    display = if(field) { MARKED_TILE } else { UNEXPLORED_TILE }
                 }
             }
         var explored: Boolean = false
             set(value) {
                 field = value
-                if(value) {
+                if (value) {
                     marked = false
                     if (mined) {
                         display = MINE_TILE
                     } else if (numMines > 0) {
                         display = numMines.digitToChar()
                     } else {
-                        display = EXPLORED_TILE
+                        display = SAFE_TILE
                     }
                 }
             }
 
     }
 
-    private var board: MutableList<MutableList<Tile>> = MutableList(X_SIZE){ MutableList(Y_SIZE){Tile(0,0)} }
-    var mineLocations: MutableList<MutableList<Int>> = MutableList(0) {
-        MutableList(2){0}
+    private var board: MutableList<MutableList<Tile>> = MutableList(X_SIZE) { MutableList(Y_SIZE) { Tile(0, 0) } }
+    var mineCoordinates: MutableList<MutableList<Int>> = MutableList(0) {
+        MutableList(2) { 0 }
+    }
+    var markedCoordinates: MutableList<MutableList<Int>> = MutableList(0) {
+        MutableList(2) { 0 }
     }
 
     init {
         board
-        for(xi in 0..X_SIZE) {
-            for(yi in 0..Y_SIZE) {
+        for (xi in 0 until X_SIZE) {
+            for (yi in 0 until Y_SIZE) {
                 board[xi][yi] = Tile(xi, yi)
             }
         }
 
         var i = 0
-        while(i < numMines) {
+        while (i < numMines) {
             val randX = Random.nextInt(0 until X_SIZE)
             val randY = Random.nextInt(0 until Y_SIZE)
 
-            if(isSafe(randX, randY)) {
+            if (isSafe(randX, randY)) {
                 placeMine(randX, randY)
                 i++
             }
@@ -119,21 +114,29 @@ class MinesweeperBoard(val numMines: Int, val X_SIZE: Int = 9, val Y_SIZE: Int =
     }
 
     fun isSafe(x: Int, y: Int): Boolean {
+        return !board[x][y].mined
+    }
+
+    fun isMined(x: Int, y: Int): Boolean {
         return board[x][y].mined
+    }
+
+    fun isExplored(x: Int, y: Int): Boolean {
+        return board[x][y].explored
     }
 
     private fun placeMine(x: Int, y: Int) {
         board[x][y].mined = true
-        mineLocations.add(mutableListOf(x, y))
+        mineCoordinates.add(mutableListOf(x, y))
     }
 
     fun displayBoard() {
         println("\n |123456789|")
         println("-|----------|")
         var i = 1
-        for(mList in board) {
+        for (mList in board) {
             print("$i|")
-            for(tile in mList) {
+            for (tile in mList) {
                 print(tile.display)
             }
             i++
@@ -142,10 +145,81 @@ class MinesweeperBoard(val numMines: Int, val X_SIZE: Int = 9, val Y_SIZE: Int =
         println("-|---------|")
     }
 
+    fun markSpot(x: Int, y: Int): Boolean {
+        if (isValidCoordinate(x, y)) {
+            if (!board[x][y].marked) {
+                board[x][y].marked = true
+                markedCoordinates.add(mutableListOf(x, y))
+            } else {
+                board[x][y].marked = false
+                markedCoordinates.remove(mutableListOf(x, y))
+            }
+        }
+
+        return calculateWin()
+    }
+
+    /**
+     * return false if mine is found
+     */
+    fun explore(x: Int, y: Int): Boolean {
+        if (isValidCoordinate(x, y)) {
+            if (isMined(x, y)) {
+                for(tile in mineCoordinates) {
+                    board[tile[0]][tile[1]].explored = true
+                }
+                return false
+            }
+            autoExplore(x, y)
+        }
+        return true
+    }
+
+    private fun autoExplore(x: Int, y: Int) {
+        if (isValidCoordinate(x, y)) {
+            if (!isExplored(x, y)) {
+                board[x][y].explored = true
+                if(board[x][y].marked) {
+                    markedCoordinates.remove(mutableListOf(x,y))
+                }
+
+                if (board[x][y].numMines == 0) {
+                    autoExplore(x - 1, y - 1)
+                    autoExplore(x - 1, y)
+                    autoExplore(x - 1, y + 1)
+                    autoExplore(x, y - 1)
+                    autoExplore(x, y + 1)
+                    autoExplore(x + 1, y - 1)
+                    autoExplore(x + 1, y)
+                    autoExplore(x + 1, y + 1)
+                }
+            }
+        }
+    }
+
+    fun isValidCoordinate(x: Int, y: Int): Boolean {
+        return x in 0 until X_SIZE && y in 0 until Y_SIZE
+    }
+
+    fun calculateWin(): Boolean {
+        if (markedCoordinates.containsAll(mineCoordinates) && mineCoordinates.containsAll(markedCoordinates)) {
+            return true
+        }
+        for (mList in board) {
+            for (tile in mList) {
+                if (!tile.explored && !tile.mined) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
 
     private fun calcNumMinesAll() {
-        for(xi in 0 until X_SIZE) {
-            for(yi in 0 until Y_SIZE) {
+        for (xi in 0 until X_SIZE) {
+            for (yi in 0 until Y_SIZE) {
                 calcNumMines(xi, yi)
             }
         }
@@ -153,7 +227,7 @@ class MinesweeperBoard(val numMines: Int, val X_SIZE: Int = 9, val Y_SIZE: Int =
 
     private fun calcNumMines(x: Int, y: Int) {
         var numMines: Int = 0
-        if (oldIsSafe(x, y)) {
+        if (isSafe(x, y)) {
             if (x > 0) {
                 if (y > 0) {
                     if (isMined(x - 1, y - 1)) {
@@ -179,204 +253,29 @@ class MinesweeperBoard(val numMines: Int, val X_SIZE: Int = 9, val Y_SIZE: Int =
                     numMines++
                 }
             }
-            if(x + 1 < X_SIZE) {
-                if(y > 0) {
-                    if(isMined(x + 1, y - 1)) {
-                        numMines++
-                    }
-                }
-                if(isMined(x + 1, y)) {
-                    numMines++
-                }
-                if(y + 1 < Y_SIZE) {
-                    if(isMined(x + 1, y + 1)) {
-                        numMines++
-                    }
-                }
-            }
-
-            if(numMines > 0) {
-                board[x][y].numMines = numMines
-            }
-
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    val MINE_TILE: Char = 'x'
-    val SAFE_TILE: Char = '.'
-    val MARKED_TILE: Char = '*'
-
-
-
-
-
-    var oldBoard: MutableList<MutableList<Char>> = MutableList(9) {
-            MutableList(9) {SAFE_TILE}
-    }
-
-    var visibleBoard: MutableList<MutableList<Char>> = oldBoard
-
-    fun oldDisplayBoard() {
-        println("\n |123456789|")
-        println("-|----------|")
-        var i = 1
-        for(mList in visibleBoard) {
-            print("$i|")
-            for(tile in mList) {
-                print(tile)
-            }
-            i++
-            println("|")
-        }
-        println("-|---------|")
-    }
-
-    private fun oldPlaceMine(x: Int, y: Int) {
-        oldBoard[x][y] = MINE_TILE
-        mineLocations.add(mutableListOf(x, y))
-    }
-
-    fun isMined(x: Int, y: Int): Boolean {
-        return oldBoard[x][y] == MINE_TILE
-    }
-
-    fun oldIsSafe(x: Int, y: Int): Boolean {
-        return oldBoard[x][y] != MINE_TILE
-    }
-
-
-
-    fun oldcreateBoard(n: Int) {
-        oldBoard = MutableList(Y_SIZE) {
-            MutableList(X_SIZE) {SAFE_TILE}
-        }
-
-
-        var i = 0
-        while (i < n) {
-            val randX = Random.nextInt(0 until X_SIZE)
-            val randY = Random.nextInt(0 until Y_SIZE)
-
-            if(oldIsSafe(randX, randY)) {
-                oldPlaceMine(randX, randY)
-                i++
-            }
-        }
-
-        oldcalcNumMinesAll()
-
-        visibleBoard = oldBoard
-
-        removeMinesFromVisible()
-    }
-
-    private fun removeMinesFromVisible() {
-        for(mine in mineLocations) {
-            visibleBoard[mine[0]][mine[1]] = SAFE_TILE
-        }
-    }
-
-    fun markMineInVisible(x: Int, y: Int) {
-        when {
-            visibleBoard[x][y] == SAFE_TILE -> {
-                visibleBoard[x][y] = MARKED_TILE
-            }
-            visibleBoard[x][y] == MARKED_TILE -> {
-                visibleBoard[x][y] = SAFE_TILE
-            }
-            else -> {
-                println("There is a number number here!")
-            }
-        }
-    }
-
-
-
-    private fun oldcalcNumMinesAll() {
-        for(xi in 0 until X_SIZE) {
-            for(yi in 0 until Y_SIZE) {
-                oldcalcNumMines(xi, yi)
-            }
-        }
-    }
-
-    private fun oldcalcNumMines(x: Int, y: Int) {
-        var numMines: Int = 0
-        if (oldIsSafe(x, y)) {
-            if (x > 0) {
+            if (x + 1 < X_SIZE) {
                 if (y > 0) {
-                    if (isMined(x - 1, y - 1)) {
+                    if (isMined(x + 1, y - 1)) {
                         numMines++
                     }
                 }
-                if (isMined(x - 1, y)) {
+                if (isMined(x + 1, y)) {
                     numMines++
                 }
                 if (y + 1 < Y_SIZE) {
-                    if (isMined(x - 1, y + 1)) {
+                    if (isMined(x + 1, y + 1)) {
                         numMines++
                     }
                 }
             }
-            if (y > 0) {
-                if (isMined(x, y - 1)) {
-                    numMines++
-                }
-            }
-            if (y + 1 < Y_SIZE) {
-                if (isMined(x, y + 1)) {
-                    numMines++
-                }
-            }
-            if(x + 1 < X_SIZE) {
-                if(y > 0) {
-                    if(isMined(x + 1, y - 1)) {
-                        numMines++
-                    }
-                }
-                if(isMined(x + 1, y)) {
-                    numMines++
-                }
-                if(y + 1 < Y_SIZE) {
-                    if(isMined(x + 1, y + 1)) {
-                        numMines++
-                    }
-                }
-            }
-
-            if(numMines > 0) {
-                oldBoard[x][y] = numMines.digitToChar()
-            }
-
         }
+
+        board[x][y].numMines = numMines
     }
+
 }
+
+
+
+
+
